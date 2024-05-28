cfbb0dad36106414fe2f1a2a31d6035e64072537
Matheus Vidal <matheusvidal140@gmail.com>
2024-05-28 10:11:46 -0300

    Initial commit

---
+++ cmd/consumer/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/matheusvidal21/go-events-rabbitmq/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, msgs, "minhafila")
+
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
---
+++ cmd/producer/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/matheusvidal21/go-events-rabbitmq/pkg/rabbitmq"
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	rabbitmq.Publish(ch, "Hello World!", "amq.direct")
+
+}
